c925392f176541572f20d2012b974da65dce2b1d
mariusz <marcinka2205@o2.pl>
2015-04-18 22:04:37 +0200

    Dashboard initial commit

--- app/controllers/Dashboard.scala
+++ app/controllers/Dashboard.scala
@@ -1,28 +1,11 @@
 package controllers
 
-import play.api.data.Form
-import play.api.data.Forms._
 import play.api.mvc.{Action, Controller}
 
-/**
- * @author mariusz marciniak
- */
 object Dashboard extends Controller {
 
-  case class User(name: String)
-
-  val tmpForm: Form[User] = Form(
-    mapping(
-      "login" -> nonEmptyText
-    ) (User.apply)(User.unapply)
-  )
-
-  def register = Action {
-    Ok("xxx")
-  }
-
   def mainPage = Action {
-    Ok(views.html.dashboard(tmpForm))
+    Ok(views.html.dashboard())
   }
 
 }
